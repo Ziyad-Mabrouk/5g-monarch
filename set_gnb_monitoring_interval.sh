6c3c50db26cbc25905dbaaca0ab70bd59c57b8e5
Ziyad Mabrouk <ziyadmabrouk@outlook.com>
2025-06-20 14:51:24 +0200

    added script to set gnb log writing frequency

--- set_gnb_monitoring_interval.sh
+++ set_gnb_monitoring_interval.sh
@@ -1,24 +1,35 @@
 #!/bin/bash
 
-INTERVAL=$(grep '^MONARCH_MONITORING_INTERVAL=' .env | cut -d '=' -f2 | tr -d '"')
+# Check if interval is provided as argument
+if [[ -n "$1" ]]; then
+  INTERVAL="$1"
+else
+  # Read from .env if no argument provided
+  INTERVAL=$(grep '^MONARCH_MONITORING_INTERVAL=' .env | cut -d '=' -f2 | tr -d '"')
+fi
+
 
 if [[ "$INTERVAL" == *ms ]]; then
   MS=${INTERVAL%ms}
 elif [[ "$INTERVAL" == *s ]]; then
-  MS=$(( ${INTERVAL%s} * 1000 ))
+  SECONDS=${INTERVAL%s}
+  if ! [[ "$SECONDS" =~ ^[0-9]+$ ]]; then
+    echo "Invalid interval format: $INTERVAL"
+    exit 1
+  fi
+  MS=$((SECONDS * 1000))
 else
   echo "Unsupported MONARCH_MONITORING_INTERVAL format: $INTERVAL"
+  echo "   Supported formats: <int>ms or <int>s"
   exit 1
 fi
 
-# Find the oai-flexric pod
 POD=$(kubectl get pods -n open5gs -o name | grep oai-flexric | head -n1 | cut -d '/' -f2)
 
 if [[ -z "$POD" ]]; then
-  echo "Could not find oai-flexric pod in 'monarch' namespace"
+  echo "Could not find oai-flexric pod in 'open5gs' namespace"
   exit 1
 fi
 
-# Execute xapp_mac_ctrl with the parsed interval
-echo "Setting gNB log interval to ${MS}ms using OAI-FlexRIC"
+echo "Setting gNB log interval to ${MS}ms using OAI-FlexRIC (pod: $POD)"
 kubectl exec -n open5gs "$POD" -- bash -c "/usr/local/flexric/xApp/c/ctrl/xapp_mac_ctrl $MS"
