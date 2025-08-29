386a15e5791f4687f04e996e4239d22dc8314a68
Ziyad Mabrouk <ziyadmabrouk@outlook.com>
2025-08-29 11:23:04 +0200

    fix thanos installation

--- data_distribution/install.sh
+++ data_distribution/install.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 THANOS_VER="12.4.3"
-HELM_REPO_URL="https://charts.bitnami.com/bitnami"
-HELM_REPO_NAME="bitnami"
+HELM_REPO_URL="oci://registry-1.docker.io/bitnamicharts"
 NAMESPACE="monarch"
 MODULE_NAME="datadist"
 
 set -o allexport; source ../.env; set +o allexport
 
 kubectl get namespace $NAMESPACE 2>/dev/null || kubectl create namespace $NAMESPACE
-helm repo add $HELM_REPO_NAME $HELM_REPO_URL || echo "Helm repo $HELM_REPO_NAME already exists."
-helm repo update
 
-
-envsubst < thanos-values.yaml | helm upgrade --install $MODULE_NAME $HELM_REPO_NAME/thanos \
+envsubst < thanos-values.yaml | helm upgrade --install $MODULE_NAME $HELM_REPO_URL/thanos \
         --namespace $NAMESPACE \
         --version $THANOS_VER \
         -f -
